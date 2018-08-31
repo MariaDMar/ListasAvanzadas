4f1b97e7bfd13dd89ec1934d8af7ceb21236ffa8
Isabella Jordan <isabellajordanbe@gmail.com>
2018-08-31 15:07:28 -0500

    changes

--- app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/MainActivity.java
@@ -4,18 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lv_noticias;
     NoticiaAdapter customAdapter;
+    EditText et_titulo;
+    Button btn_generar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_titulo = findViewById(R.id.et_titulo);
+        btn_generar = findViewById(R.id.btn_generar);
 
         lv_noticias = findViewById(R.id.lv_noticias);
         customAdapter = new NoticiaAdapter(this);
@@ -25,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
                 new Noticia("HED","Habrá un cambio en el logo de hoy es diseño","30/08/2018");
         Noticia noticia2 =
                 new Noticia("ESTA","Tendra una nueva noticia","28/08/2018");
+
         customAdapter.agregarNoticia(noticia1);
+        customAdapter.agregarNoticia(noticia2);
+
         lv_noticias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -34,7 +46,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+//noticia random
+        btn_generar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                //traer la fecha actual del sistema
+
+                Calendar c = Calendar.getInstance();
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                month++;
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                String fecha = day+"/"+month+"/"+year;
+
+                String titulo = et_titulo.getText().toString();
+                Noticia newNoticia = new Noticia(titulo,"no descripcion", fecha);
+                customAdapter.agregarNoticia(newNoticia);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
+++ app/src/main/java/com/example/estudiante/listasavanzadas/NoticiaAdapter.java
@@ -1,11 +1,14 @@
 package com.example.estudiante.listasavanzadas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.zip.Inflater;
@@ -40,7 +43,7 @@ public class NoticiaAdapter extends BaseAdapter {
 
     //Generar un renglón por objeto
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = activity.getLayoutInflater();
         //pasa de xml a view
 
@@ -48,10 +51,29 @@ public class NoticiaAdapter extends BaseAdapter {
         TextView item_titulo = renglon.findViewById(R.id.item_titulo);
         TextView item_fecha = renglon.findViewById(R.id.item_fecha);
         TextView item_descripcion = renglon.findViewById(R.id.item_descripcion);
+        Button item_action = renglon.findViewById(R.id.item_action);
 
         item_titulo.setText(noticias.get(position).getTitulo());
         item_fecha.setText(noticias.get(position).getFecha());
         item_descripcion.setText(noticias.get(position).getDescripcion());
+        item_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(activity,"POS " +position, Toast.LENGTH_SHORT).show();
+
+                //  noticias.remove(position); //este es para eliminar
+
+                //  notifyDataSetChanged(); //eso actualiza o redenrizar again
+
+                //llamar otra actividad dentro de un item:
+                Intent intent = new Intent(activity, NoticiaView.class);
+                activity.startActivity(intent);
+
+                //esto es lo que hariamos si estuvieramos parados enla otra clase
+                //Intent i = new Intent(MainActivity.this, NoticiaView.class);
+                //startActivity(i);
+            }
+        });
 
         return renglon;
     }
@@ -59,6 +81,7 @@ public class NoticiaAdapter extends BaseAdapter {
     public void agregarNoticia(Noticia noticia){
         noticias.add(noticia);
         notifyDataSetChanged();
-
     }
+
+
 }
